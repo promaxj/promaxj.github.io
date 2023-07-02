fdbd44248488281abfb590d28c7cac092951934b
zelig <promaxj@users.noreply.github.com>
2023-07-02 14:44:23 +0800

    web

--- script.js
+++ script.js
@@ -4,4 +4,5 @@ function drawLottery() {
   
     var randomNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
     document.getElementById("result").innerHTML = "抽到的數字是：" + randomNumber;
+    resultElement.style.fontSize = "36px"; // 將字體大小設定為36px
   }
