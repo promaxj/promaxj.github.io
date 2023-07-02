4a54e9f4bc60a5c8f8afd4c976150b9cb4440eac
zelig <promaxj@users.noreply.github.com>
2023-07-02 14:51:21 +0800

    web

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function drawLottery() {
-    var minNumber = 1; // 最小數字
-    var maxNumber = 100; // 最大數字
+    var minNumber = parseInt(document.getElementById("min").value);
+    var maxNumber = parseInt(document.getElementById("max").value);
   
     var randomNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
-    document.getElementById("result").innerHTML = "抽到的數字是：" + randomNumber;
-    resultElement.style.fontSize = "36px"; // 將字體大小設定為36px
+    var resultElement = document.getElementById("result");
+    resultElement.innerHTML = "抽到的數字是：" + randomNumber;
+    resultElement.style.fontSize = "72px"; // 將字體大小設定為36px
   }
+  
