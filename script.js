983ff18435cf820da298a6c7ea83c412c4d46d01
zelig <promaxj@users.noreply.github.com>
2023-07-02 14:53:30 +0800

    web

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ function drawLottery() {
     var minNumber = parseInt(document.getElementById("min").value);
     var maxNumber = parseInt(document.getElementById("max").value);
   
+    if (isNaN(minNumber) || isNaN(maxNumber)) {
+      alert("請輸入有效的數字！");
+      return;
+    }
+  
+    if (minNumber > maxNumber) {
+      alert("最小值不得大於最大值！");
+      return;
+    }
+  
     var randomNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
     var resultElement = document.getElementById("result");
     resultElement.innerHTML = "抽到的數字是：" + randomNumber;
