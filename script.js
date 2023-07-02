0d45426594d58d9c6c05e38c70cadde06a06ee72
zelig <promaxj@users.noreply.github.com>
2023-07-02 14:55:28 +0800

    web

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function drawLottery() {
     var minNumber = parseInt(document.getElementById("min").value);
     var maxNumber = parseInt(document.getElementById("max").value);
+    var excludeNumbersInput = document.getElementById("exclude").value;
+    var excludeNumbers = excludeNumbersInput.split(",").map(function(number) {
+      return parseInt(number.trim());
+    });
   
     if (isNaN(minNumber) || isNaN(maxNumber)) {
       alert("請輸入有效的數字！");
@@ -12,7 +16,16 @@ function drawLottery() {
       return;
     }
   
-    var randomNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+    if (excludeNumbers.some(function(number) { return isNaN(number); })) {
+      alert("請輸入有效的排除數字！");
+      return;
+    }
+  
+    var randomNumber;
+    do {
+      randomNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+    } while (excludeNumbers.includes(randomNumber));
+  
     var resultElement = document.getElementById("result");
     resultElement.innerHTML = "抽到的數字是：" + randomNumber;
     resultElement.style.fontSize = "72px"; // 將字體大小設定為36px
