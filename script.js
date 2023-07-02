3ea9368de3863cea3374830380ba58edef41dc16
zelig <promaxj@users.noreply.github.com>
2023-07-02 14:38:20 +0800

    web

---
+++ script.js
@@ -0,0 +1,7 @@
+function drawLottery() {
+    var minNumber = 1; // 最小數字
+    var maxNumber = 100; // 最大數字
+  
+    var randomNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+    document.getElementById("result").innerHTML = "抽到的數字是：" + randomNumber;
+  }
